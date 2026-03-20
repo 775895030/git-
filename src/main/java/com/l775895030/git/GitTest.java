e5d36b31df2c96b3b2d78c445e1359df8a22481f
Eason <775895030@qq.com>
2026-03-20 10:24:37 +0800

    second commit

--- src/main/java/com/l775895030/git/GitTest.java
+++ src/main/java/com/l775895030/git/GitTest.java
@@ -3,5 +3,6 @@ package com.l775895030.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
